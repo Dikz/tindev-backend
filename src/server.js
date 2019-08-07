ef64b25aa02c150c1d0533d652f8d37b3da0545b
Dikz <cicerogabriel12@hotmail.com>
2019-08-07 17:29:43 -0300

    First Commit

---
+++ src/server.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+
+const routes = require('./routes')
+
+const server = express()
+
+mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_URL}`, {
+	useNewUrlParser: true
+})
+
+server.use(cors())
+server.use(express.json())
+server.use(routes)
+
+server.listen(process.env.PORT || 3333)
